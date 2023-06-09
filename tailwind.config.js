36d9ae16f609bd682ab2404171dc0f3553aa195b
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-06-09 11:52:46 +0100

    tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        skin: {
+          base: 'var(--color-bg-base)',
+          secondary: 'var(--color-bg-secondary)',
+          input: 'var(--color-bg-input)',
+        },
+    },
+  },
+  plugins: [],
+ }
+}
