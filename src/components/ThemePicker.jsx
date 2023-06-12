0e09dc9b0806de1a900947698b595b7ac2d81a06
Grego12098 <jgregory12098@gmail.com>
2023-06-12 14:30:02 +0100

    FIXED IT

--- src/components/ThemePicker.jsx
+++ src/components/ThemePicker.jsx
@@ -1,22 +1,12 @@
 export default function ThemePicker({onThemeChange}) {
-    return (
-
-      <div className="flex flex-col">
+  return (
+    <div className="flex flex-col">
       <button onClick={() => onThemeChange('')}>🎨</button>
       <button className="m-4" onClick={() => onThemeChange('theme-beach')}>🏖️</button>
       <button onClick={() => onThemeChange('theme-forest')}>🌳</button>
       <button onClick={() => onThemeChange('theme-space')}>🚀</button>
       <button onClick={() => onThemeChange('theme-under_the_sea')}>🐠</button>
       <button onClick={() => onThemeChange('theme-stars')}>⭐</button>
-
-      <div className="flex justify-center color text-skin-muted">
-      <button onClick={() => onThemeChange('')}>Clouds</button>
-      <button className="m-4" onClick={() => onThemeChange('theme-beach')}>Beach</button>
-      <button onClick={() => onThemeChange('theme-forest')}>Forest</button>
-      <button onClick={() => onThemeChange('theme-space')}>Space</button>
-      <button onClick={() => onThemeChange('theme-under_the_sea')}>SSeapace</button>
-      <button onClick={() => onThemeChange('theme-stars')}>Stars</button>
-
-      </div>
+    </div>
       );
-  }
+}
