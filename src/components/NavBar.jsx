89794af967056b150b0e54d3b5122aaf87ab71e7
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-06-12 11:12:45 +0100

    m

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import ThemePicker from "./ThemePicker";
+
 export default function NavBar({ handleThemeChange }) {
     return (
       // pass the handleThemeChange function to the ThemePicker component via props
