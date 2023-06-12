9c1a3d025b3d4ca86c0bc8484cba2741571252d4
Grego12098 <jgregory12098@gmail.com>
2023-06-12 10:58:12 +0100

    super duper helpful comments in AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -7,8 +7,10 @@ export default function AppLayout({ handleThemeChange }) {
   return (
     <div>
       <div className="bg-skin-base">
+        {/* you should only need to use bg-skin-base bg-skin-secondary etc for header colours, no need to use the theme variable */}
         <NavLink to="page1">page1</NavLink>
         <NavLink to="page2">page2</NavLink>
+        {/* style container for all pages here between main JSX element tags */}
         <main>
           <Outlet />
         </main>
