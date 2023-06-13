a52288d202eb96bdc8cc6f317a7a685b975cdd39
Guy Mulligan <guymulligan@outlook.com>
2023-06-13 16:16:04 +0100

    format better

--- src/pages/AddEntry.jsx
+++ src/pages/AddEntry.jsx
@@ -2,22 +2,22 @@ export default function AddEntry() {
     return (
     <div>
         <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base'>What made you smile today?</label>
-        <input className ='bg-skin-input shadow-md'></input>
+        <label className = 'text-sm sm:text-base py-2 px-4'>What made you smile today?</label>
+        <input className ='bg-skin-input shadow-md py-8 px-3 mx-4'></input>
         </div>
 
         <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base'>What did you find challenging today?</label>
-        <input className ='bg-skin-input shadow-md'></input>
+        <label className = 'text-sm sm:text-base py-2 p-4'>What did you find challenging today?</label>
+        <input className ='bg-skin-input shadow-md py-8 px-3 mx-4'></input>
         </div>
 
         <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base'>If your mood was a type of weather, what would it be? Sunny, cloudy, stormy or something else?</label>
-        <input className ='bg-skin-input shadow-md'></input>
+        <label className = 'text-sm sm:text-base py-2 px-4'>If your mood was a type of weather, what would it be? Sunny, cloudy, stormy or something else?</label>
+        <input className ='bg-skin-input shadow-md py-8 px-3 mx-4'></input>
         </div>
 
         <div className = 'flex justify-center'>
-        <button className="text-white shadow-md text-2xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-1.5 px-3">Post!</button>
+        <button className="text-white shadow-md text-2xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-1.5 px-3 m-6">Post!</button>
     </div>
     </div>
     );
