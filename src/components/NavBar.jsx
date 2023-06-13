1b5c51886ceedad0e2b5671e8076d8d409a7ee3a
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-06-13 11:12:30 +0100

    navbar finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import ThemePicker from "./ThemePicker";
 import { useState } from "react";
 import { NavLink } from 'react-router-dom';
+/* Import the font awesome library here */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus, faBook, faTemperatureHalf,faPalette } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -12,17 +15,41 @@ export default function NavBar({ handleThemeChange }) {
   };
   return (
     <div className="flex justify-center align-center">
+      {/* nav bar white box start here */}
     <div className="flex justify-around fixed bottom-3 w-9/12  h-12  bg-white rounded-lg">
-      <NavLink to="AddEntry" className="flex items-center">📝</NavLink>
-      <NavLink to="Journal" className="flex items-center">📖</NavLink>
-      <NavLink to="MoodMap" className="flex items-center">🗺️</NavLink>
+      <NavLink to="AddEntry" className="flex items-center ">
+        {/* Icon imported from Font Awesome */}
+        <FontAwesomeIcon 
+        icon={faCirclePlus}
+        className="w-8 h-8"
+        style={{ color: "var(--color-icon-secondary)" }} />
+        </NavLink>
+      <NavLink to="Journal" className="flex items-center">
+        <FontAwesomeIcon 
+        icon={faBook}
+        className="w-7 h-7"
+        style={{ color: "var(--color-icon-secondary)" }}
+        />
+        </NavLink>
+      <NavLink to="MoodMap" className="flex items-center">
+        <FontAwesomeIcon 
+        icon={faTemperatureHalf}
+        className="w-8 h-8"
+        style={{ color: "var(--color-icon-secondary)" }}
+         />
+        </NavLink>
     <div className="relative flex items-center">
       <button onClick={toggleThemePicker}>
-        {/* Add your button image here */}
-        🎨
+        {/* Clickeanble palete*/}
+        <FontAwesomeIcon 
+        icon={faPalette}
+        className="w-7 h-7" 
+        style={{ color: "var(--color-icon-secondary)" }}
+        />
       </button>
+      {/* work in a future transition */}
       {isThemePickerOpen && (
-        <div className="absolute bottom-0 right-0 mb-8">
+        <div className="absolute justify-center align-center bottom-5 right-0.1 mb-8 ">
         <ThemePicker onThemeChange={handleThemeChange} />
         </div>
       )}
@@ -31,5 +58,3 @@ export default function NavBar({ handleThemeChange }) {
     </div>
   );
 }
-  
-// mx-auto w-60%
