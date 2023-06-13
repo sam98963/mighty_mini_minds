a9f4338045c0b7920632285a9cb0a9ffd395891e
Grego12098 <jgregory12098@gmail.com>
2023-06-13 15:57:25 +0100

    nothing really

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -8,14 +8,13 @@ export default function AppLayout({ handleThemeChange }) {
 
   return (
     <div>
-        <Header />
+      <Header />
         {/* style container for all pages here between main JSX element tags */}
-        <main className = 'flex justify-center '>
-        <div className='w-11/12 sm:w-9/12 lg:w-8/12 xl:w-7/12 mt-4 bg-white rounded-lg shadow-lg min-h-[80%]'>
+      <main className = 'flex justify-center min-h-4/5'>
+        <div className='w-11/12 sm:w-9/12 lg:w-8/12 xl:w-7/12 mt-4 bg-white rounded-lg shadow-lg'>
           <Outlet />
-          </div>
-  
-        </main >
+        </div>
+      </main >
       <NavBar handleThemeChange={handleThemeChange} />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,10 @@ export default {
           secondary: 'var(--color-bg-secondary)',
           input: 'var(--color-bg-input)',
         },
-    },
-      dropShadow: {
-        'input': '0 0 2px rgba(0, 0, 0, 0.3)'
       },
+    minHeight: {
+      '4/5': '80%',
+    },
   },
   plugins: [],
  }
