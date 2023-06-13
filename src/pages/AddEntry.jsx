2dd50fefce1a17ce19081fe2ea1ba8423aecd56d
Guy Mulligan <guymulligan@outlook.com>
2023-06-13 14:56:17 +0100

    big chungus button and making white div bac kground

--- src/pages/AddEntry.jsx
+++ src/pages/AddEntry.jsx
@@ -1,7 +1,24 @@
 export default function AddEntry() {
     return (
-        <div>
-        <h1>Add Entry</h1>
+    <div>
+        <div className = 'flex flex-col'>
+        <label className = 'text-sm sm:text-base'>What made you smile today?</label>
+        <input className ='bg-skin-input shadow-md'></input>
         </div>
+
+        <div className = 'flex flex-col'>
+        <label className = 'text-sm sm:text-base'>What did you find challenging today?</label>
+        <input className ='bg-skin-input shadow-md'></input>
+        </div>
+
+        <div className = 'flex flex-col'>
+        <label className = 'text-sm sm:text-base'>If your mood was a type of weather, what would it be? Sunny, cloudy, stormy or something else?</label>
+        <input className ='bg-skin-input shadow-md'></input>
+        </div>
+
+        <div className = 'flex justify-center'>
+        <button className="text-white shadow-md text-4xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-3 px-5">Post!</button>
+    </div>
+    </div>
     );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -7,11 +7,14 @@ import '../App.css';
 export default function AppLayout({ handleThemeChange }) {
 
   return (
-    <div className='maindiv'>
+    <div>
         <Header />
         {/* style container for all pages here between main JSX element tags */}
-        <main>
+        <main className = 'flex justify-center '>
+        <div className='w-11/12 sm:w-9/12 lg:w-8/12 xl:w-7/12 mt-4 bg-white rounded-lg shadow-lg min-h-[80%]'>
           <Outlet />
+          </div>
+  
         </main >
       <NavBar handleThemeChange={handleThemeChange} />
     </div>
