b48f34974f9e3a6c04e9048a02ce9b8574dff140
Grego12098 <jgregory12098@gmail.com>
2023-06-14 16:09:04 +0100

    setting up tanstack/query

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import HomePage from "./pages/HomePage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
@@ -40,10 +44,14 @@ function App() {
       </Route>
     )
   );
+  // set up react-query
+  const queryClient = new QueryClient();
 
   return (
     <div className={`bg-skin-base ${theme} min-h-screen overflow-hidden`}>
-      <RouterProvider router={router}/>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+      </QueryClientProvider>
     </div>
   );
 }
