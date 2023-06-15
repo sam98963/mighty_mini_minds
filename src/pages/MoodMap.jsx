94ae90856815c933aaeafb8c35716875387284ab
Guy Mulligan <guymulligan@outlook.com>
2023-06-15 14:53:34 +0100

    good luck

--- src/pages/MoodMap.jsx
+++ src/pages/MoodMap.jsx
@@ -1,57 +1,57 @@
- import Avatar2 from "../components/Avatar";
+import Avatar2 from "../components/Avatar";
 
 export default function MoodMap() {
   return (
-    <div className="flex flex-col justify-start items-center h-screen">
-      <h1 className="text-2xl sm:text-4xl text-center px-4 mb-10 mt-4">
+    <div className="flex flex-col justify-around items-center w-full h-full">
+      <h1 className="text-4xl sm:text-4xl text-center px-4 mb-10 mt-4">
         My Week
       </h1>
-      <div className="flex justify-evenly  w-full max-w-screen-lg">
+      <div className="flex justify-between">
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Mon</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Mon</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             😊
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Tue</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Tue</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             🙁
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Wed</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Wed</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             🙃
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Thu</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Thu</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             😐
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Fri</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Fri</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             😢
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Sat</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Sat</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             😁
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="text-lg sm:text-2xl text-center px-4">Sun</p>
-          <p className="text-6xl sm:text-5xl transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="text-md sm:text-sm text-center px-4">Sun</p>
+          <p className="text-2xl sm:text-md transition-all duration-300 ease-in-out transform hover:scale-125">
             😍
           </p>
         </div>
@@ -74,7 +74,5 @@ export default function MoodMap() {
        </div>
         </div>
       </div>
-
-    
   );
 }
