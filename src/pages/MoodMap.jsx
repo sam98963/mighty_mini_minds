0119ea1e7db1fbd8fb7e7750a4f3746e3f491ff6
Grego12098 <jgregory12098@gmail.com>
2023-06-16 10:48:09 +0100

    placeholder thermometer

--- src/pages/MoodMap.jsx
+++ src/pages/MoodMap.jsx
@@ -1,5 +1,6 @@
 import Avatar from "../components/Avatar";
 import Emoji from "../components/Emoji";
+import thermometer from "../Img/thermometer.png";
 export default function MoodMap() {
   return (
     <div className="flex flex-col justify-around items-center w-full h-full">
@@ -9,43 +10,43 @@ export default function MoodMap() {
       <div className="flex justify-between">
         <Emoji />
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Tue</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Tue</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             🙁
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Wed</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Wed</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             🙃
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Thu</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Thu</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             😐
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Fri</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Fri</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             😢
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Sat</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Sat</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             😁
           </p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Sun</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          <p className="sm:text-base text-sm text-center">Sun</p>
+          <p className="sm:text-4xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
             😍
           </p>
         </div>
@@ -61,8 +62,11 @@ export default function MoodMap() {
         <p className="italic text-xs sm:text-base">
         "She has a delighted smile."
         </p>
-        <div className="flex flex-row items-end ml-96 transform -scale-x-100 ">
-          <Avatar />   
+        <div className="flex w-full justify-between mt-4">
+          <img className="w-28 sm:w-32 ml-2 sm:ml-0" src={thermometer} alt="Thermometer"></img>
+          <div className="transform -scale-x-100 mt-16 mr-4 sm:mr-0">
+            <Avatar />   
+          </div>
         </div>
       </div>
     </div>
