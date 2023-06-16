bbfff67513d388f139b9f522cfad147d4a5f299b
Grego12098 <jgregory12098@gmail.com>
2023-06-16 10:31:36 +0100

    text size on mood map responsive

---
+++ src/components/Emoji.jsx
@@ -0,0 +1,10 @@
+export default function Emoji() {
+    return (
+        <div className="flex flex-col items-center">
+            <p className="sm:text-md text-sm text-center">Mon</p>
+            <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
+          😊
+            </p>
+        </div>
+    )
+}
--- src/pages/MoodMap.jsx
+++ src/pages/MoodMap.jsx
@@ -1,19 +1,13 @@
 import Avatar from "../components/Avatar";
-
+import Emoji from "../components/Emoji";
 export default function MoodMap() {
   return (
     <div className="flex flex-col justify-around items-center w-full h-full">
-      <h1 className="text-4xl sm:text-4xl text-center px-4 mb-10 mt-4">
+      <h1 className="text-4xl sm:text-4xl text-center px-4 mt-4">
         My Week
       </h1>
       <div className="flex justify-between">
-        <div className="flex flex-col items-center">
-          <p className="sm:text-md text-sm text-center">Mon</p>
-          <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
-            😊
-          </p>
-        </div>
-
+        <Emoji />
         <div className="flex flex-col items-center">
           <p className="sm:text-md text-sm text-center">Tue</p>
           <p className="sm:text-2xl text-xl px-1 sm:px-4 transition-all duration-300 ease-in-out transform hover:scale-125">
@@ -56,23 +50,21 @@ export default function MoodMap() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center mt-6">
+      <div className="flex flex-col items-center">
         <h1 className="text-2xl">
           Word of The Day
         </h1>
         <h1 className="text-2xl text-skin-secondary">
           Delighted
         </h1>
-        <h3>Feeling or showing great pleasure</h3>
-        <h3 className="italic">
-        { "She has a delighted smile."}
-        </h3>
+        <p className="text-sm sm:text-lg">Feeling or showing great pleasure</p>
+        <p className="italic text-xs sm:text-base">
+        "She has a delighted smile."
+        </p>
         <div className="flex flex-row items-end ml-96 transform -scale-x-100 ">
-        <div>
-          <Avatar />
-          </div>      
-       </div>
+          <Avatar />   
         </div>
       </div>
+    </div>
   );
 }
