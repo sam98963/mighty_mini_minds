914569f35306122df031515c3a3c08b55e44e5f4
Grego12098 <jgregory12098@gmail.com>
2023-06-16 10:55:55 +0100

    created speechBubble component

---
+++ src/components/SpeechBubble.jsx
@@ -0,0 +1,9 @@
+export default function SpeechBubble() {
+    return (
+        <div className="rounded-xl">
+            <div className="text-center">
+                <p className="text-base">Word of The Day</p>
+            </div>
+        </div>
+    )
+}
