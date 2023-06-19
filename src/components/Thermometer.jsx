7bfabeec9f69fc1b931f9475a4f0c8e1d447b8da
sarahcrack <sarah.l.crack@googlemail.com>
2023-06-19 12:20:08 +0100

    created responsive thermometer

--- src/components/Thermometer.jsx
+++ src/components/Thermometer.jsx
@@ -1,9 +1,31 @@
+// import ThermometerPic from "../Img/Therm_final.png";
 
+// export default function Thermometer(props) {
+//   return (
+//     <div className="h-60 w-24">
+//       <img
+//         src={ThermometerPic}
+//         alt="thermometer"
+//         className="h-full w-full z-10"
+//       />
+//       <div className={`h-${props.mood} w-5 bg-black`}></div>
+
+//       {/* <div className="h-20 w-5 bg-black"></div> */}
+//     </div>
+//   );
+// }
+import ThermometerPic from "../Img/Therm_final.png";
 
 export default function Thermometer(props) {
-    return(
-        <div className="h-full w-10 bg-red-500 flex flex-row justify-center items-end">
-            <div className={`h-${props.mood} w-5 bg-black`}></div>
-        </div>
-    )
+  return (
+    <div className="h-60 w-20 relative">
+      <div className="h-32 w-10/12 bg-green-300 absolute bottom-2 left-0 right-0 mx-auto z-0"></div>
+      <img
+        src={ThermometerPic}
+        alt="thermometer"
+        className="h-full w-full z-10"
+        style={{ position: "relative", zIndex: 1 }}
+      />
+    </div>
+  );
 }
--- src/pages/MoodMap.jsx
+++ src/pages/MoodMap.jsx
@@ -2,22 +2,33 @@ import Avatar from "../components/Avatar";
 import Emoji from "../components/Emoji";
 import Thermometer from "../components/Thermometer";
 import WordOfTheDay from "../components/WordOfTheDay";
+
 export default function MoodMap() {
   return (
     <div className="flex flex-col justify-around items-center w-full h-full">
-      <h1 className="text-4xl sm:text-4xl text-center px-4 mt-4">
-        My Week
-      </h1>
+      <h1 className="text-4xl sm:text-4xl text-center px-4 mt-4">My Week</h1>
       <div className="flex justify-between">
-        <Emoji mon={"😊"} tue={"🙁"} wed={"🙃"} thu={"😐"} fri={"😢"} sat={"😁"} sun={"😍"} />
+        <Emoji
+          mon={"😊"}
+          tue={"🙁"}
+          wed={"🙃"}
+          thu={"😐"}
+          fri={"😢"}
+          sat={"😁"}
+          sun={"😍"}
+        />
       </div>
-        <WordOfTheDay word={"Delighted"} definition={"Feeling or showing great pleasure"} example={"She has a delighted smile."}/>
+      <WordOfTheDay
+        word={"Delighted"}
+        definition={"Feeling or showing great pleasure"}
+        example={"She has a delighted smile."}
+      />
       <div className="flex w-full justify-around mt-4">
-        <Thermometer mood={60} />
-          <div className="transform -scale-x-100 mt-16 mr-4 sm:mr-0">
-            <Avatar />   
-          </div>
+        <Thermometer mood={20} />
+        <div className="transform -scale-x-100 mt-16 mr-4 sm:mr-0">
+          <Avatar />
         </div>
       </div>
+    </div>
   );
 }
