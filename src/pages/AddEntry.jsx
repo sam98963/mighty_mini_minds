1014ff1562d10c410f7b966e6063a8f8e8f4c5ed
Grego12098 <jgregory12098@gmail.com>
2023-06-20 22:11:01 +0100

    post now takes in questions and inputs from the add entry function

--- src/pages/AddEntry.jsx
+++ src/pages/AddEntry.jsx
@@ -26,54 +26,43 @@ export default function AddEntry() {
     const [challengeA, setChallengeA] = useState("")
     const [openA, setOpenA] = useState("")
 
-    function handlePositiveChange(event){
-        setPositiveA(event.target.value);
-        console.log(positiveA);
-    }
-    function handleChallengeChange(event){
-        setChallengeA(event.target.value);
-        console.log(challengeA);
-    }
-    function handleOpenChange(event){
-        setOpenA(event.target.value);
-        console.log(openA);
-    }
-
-    function submit(){
+    function submit() {
         const entry = {
-            mood: 5,
-            questionOne: "blagfgaa",
-            questionTwo: "lafgfa",
-            questionThree: "dfgfgaa",
-            answerOne: "rfgfgaaa",
-            answerTwo: "pafgfga",
-            answerThree: "hasdfsaa",
-            share: false,
-        }
-
+          mood: 5,
+          questionOne: questions[0],
+          questionTwo: questions[1],
+          questionThree: questions[2],
+          answerOne: positiveA, // Use value from positiveA input field
+          answerTwo: challengeA, // Use value from challengeA input field
+          answerThree: openA, // Use value from openA input field
+          share: false,
+        };
+    
         mutate(entry);
+      }
+    
+      return (
+        <div>
+          <div className='flex flex-col'>
+            <label className='text-sm sm:text-base py-2 px-8 mt-6'>{questions[0]}</label>
+            <input className='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e) => setPositiveA(e.target.value)}></input>
+          </div>
+    
+          <div className='flex flex-col'>
+            <label className='text-sm sm:text-base py-2 p-8 mt-6'>{questions[1]}</label>
+            <input className='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e) => setChallengeA(e.target.value)}></input>
+          </div>
+    
+          <div className='flex flex-col'>
+            <label className='text-sm sm:text-base py-2 px-8 mt-6'>{questions[2]}</label>
+            <input className='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e) => setOpenA(e.target.value)}></input>
+          </div>
+    
+          <div className='flex justify-center'>
+            <NavLink to="../thanksPage">
+              <button className="text-white shadow-md text-2xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-1.5 px-3 m-6" onClick={submit}>Post!</button>
+            </NavLink>
+          </div>
+        </div>
+      );
     }
-
-    return (
-    <div>
-        <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base py-2 px-8 mt-6'>{questions[0]}</label>
-        <input className ='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e)=>handlePositiveChange(e)}></input>
-        </div>
-
-        <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base py-2 p-8 mt-6'>{questions[1]}</label>
-        <input className ='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e)=>handleChallengeChange(e)}></input>
-        </div>
-
-        <div className = 'flex flex-col'>
-        <label className = 'text-sm sm:text-base py-2 px-8 mt-6'>{questions[2]}</label>
-        <input className ='bg-skin-input shadow-md py-8 px-3 mx-8' onChange={(e)=>handleOpenChange(e)}></input>
-        </div>
-
-        <div className = 'flex justify-center'>
-        <NavLink to="../thanksPage"><button className="text-white shadow-md text-2xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-1.5 px-3 m-6" onClick={submit}>Post!</button></NavLink>
-    </div>
-    </div>
-    );
-}
