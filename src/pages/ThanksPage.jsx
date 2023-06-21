e10519376e0b3591f5c04508fb64377956629e92
Grego12098 <jgregory12098@gmail.com>
2023-06-21 12:18:25 +0100

    thankyou page done?

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -1,8 +1,36 @@
 import avatar from "../Img/number7V3.png"
-import { NavLink } from "react-router-dom";
+import { NavLink, useOutletContext } from "react-router-dom";
 import Confetti from 'react-confetti';
-
+import { useState, useEffect } from "react";
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 export default function ThanksPage(){
+  const [questions, entryId]  = useOutletContext();
+  const [share, setShare] = useState(false);
+
+  function handleClick(event) {
+    setShare(event.target.value);
+    console.log(entryId);
+  }
+
+  function updateEntry() {
+    const entry = {
+      share: share,
+    };
+    mutate(entry);
+  }
+
+  useEffect(() => {
+      updateEntry();
+  }, [share]);
+
+    const {mutate} = useMutation({
+        mutationFn: async (entry) => {
+          const response = await axios.patch(`https://mighty-mini-minds-backend.onrender.com/entry/${entryId}`, entry);
+          return response.data;
+        },
+    });
+
     return(
        <div className="flex flex-col justify-around items-center w-full h-full overflow-hidden">
         <Confetti
@@ -24,8 +52,8 @@ export default function ThanksPage(){
         <img src={avatar} alt="avatar" className="h-30 w-32 sm:h-60 sm:w-64" />
         <h2 className="text-xl sm:text-2xl text-center px-4">Would you like to share your thoughts with someone?</h2>
         <div className="flex flex-row justify-around items-center w-full">
-        <NavLink to="../moodMap"><button className="text-white shadow-md text-4xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-3 px-5">✕</button></NavLink>
-        <NavLink to="../moodMap"><button className="text-white shadow-md text-4xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-3 px-5">✓</button></NavLink>
+        <NavLink to="../moodMap"><button onClick={handleClick} value={false} className="text-white shadow-md text-4xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-3 px-5">✕</button></NavLink>
+        <NavLink to="../moodMap"><button onClick={handleClick} value={true} className="text-white shadow-md text-4xl transition-colors duration-300 ease-in-out transform hover:scale-125 bg-skin-secondary rounded-md py-3 px-5">✓</button></NavLink>
         </div>
         </div>
     )
