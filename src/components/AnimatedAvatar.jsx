739dc0fe28949d7108034fcea98f2478cbee4cf3
sam98963 <szwilliamson01@gmail.com>
2023-06-23 09:42:16 +0100

    useEffect to allow animation to run on render

--- src/components/AnimatedAvatar.jsx
+++ src/components/AnimatedAvatar.jsx
@@ -1,45 +1,48 @@
-import bunny1 from "../Img/avatar-animations/bunny-1.png"
-import bunny2 from "../Img/avatar-animations/bunny-2.png"
-import bunny3 from "../Img/avatar-animations/bunny-3.png"
-import bunny4 from "../Img/avatar-animations/bunny-4.png"
-import bunny5 from "../Img/avatar-animations/bunny-5.png"
-import bunny6 from "../Img/avatar-animations/bunny-6.png"
+import React, { useEffect } from 'react';
+import bunny1 from '../Img/avatar-animations/bunny-1.png';
+import bunny2 from '../Img/avatar-animations/bunny-2.png';
+import bunny3 from '../Img/avatar-animations/bunny-3.png';
+import bunny4 from '../Img/avatar-animations/bunny-4.png';
+import bunny5 from '../Img/avatar-animations/bunny-5.png';
+import bunny6 from '../Img/avatar-animations/bunny-6.png';
 
-
-export default function AnimatedAvatar(){
-  const bunnies = [bunny1, bunny2, bunny3, bunny4, bunny5, bunny6]
-  const element = document.querySelector('.avatar-animation');
+export default function AnimatedAvatar() {
+  const bunnies = [bunny1, bunny2, bunny3, bunny4, bunny5, bunny6];
   const totalFrames = 5;
   const animationDuration = 2000;
   const timePerFrame = animationDuration / totalFrames;
   let timeWhenLastUpdate;
   let timeFromLastUpdate;
   let frameNumber = 0;
-  
-  function step(startTime) {
-    if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
-  
-    timeFromLastUpdate = startTime - timeWhenLastUpdate;
-  
-    if (timeFromLastUpdate > timePerFrame) {
-      element.src = bunnies[frameNumber]
-      timeWhenLastUpdate = startTime;
-  
-      if (frameNumber >= totalFrames) {
-        frameNumber = 0;
-      } else {
-        frameNumber = frameNumber + 1;
-      }        
+
+  useEffect(() => {
+    const element = document.querySelector('.avatar-animation');
+
+    function step(startTime) {
+      if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+
+      const timeFromLastUpdate = startTime - timeWhenLastUpdate;
+
+      if (timeFromLastUpdate > timePerFrame) {
+        element.src = bunnies[frameNumber];
+        timeWhenLastUpdate = startTime;
+
+        if (frameNumber >= totalFrames - 1) {
+          frameNumber = 0;
+        } else {
+          frameNumber = frameNumber + 1;
+        }
+      }
+
+      requestAnimationFrame(step);
     }
-  
+
     requestAnimationFrame(step);
-  }
-  
-  requestAnimationFrame(step);
-  return(
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
     <>
-      <img className="avatar-animation" src={bunnies[0]}>
-    </img>
+      <img className="avatar-animation" src={bunnies[0]} alt="Animated Avatar" />
     </>
-  )  
+  );
 }
