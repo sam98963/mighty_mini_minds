3d4c69f90a9bf7b861e49c4392082227a61a2e43
Grego12098 <jgregory12098@gmail.com>
2023-06-23 14:21:29 +0100

    added random quotes back in for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import logo from "../Img/logo-close.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
@@ -64,6 +64,18 @@ export default function Login() {
     }
   }
 
+  const [randomQuote, setRandomQuote] = useState(null);
+  useEffect(() => {
+    function generateRandomQuote() {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const selectedQuote = quotes[randomIndex];
+      console.log(selectedQuote);
+      return selectedQuote;
+    }
+    const quote = generateRandomQuote();
+    setRandomQuote(quote);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-around h-screen">
     <img src={logo} alt="logo" className="h-28 w-44" />
@@ -71,10 +83,14 @@ export default function Login() {
       <h1 className="font-bold text-center text-4xl mb-12">
         Mighty Mini Minds
       </h1>
-      <p className="text-xl font-semibold">“I’m enough as I am.” - Boy</p>
-      <p className="italic mt-2">
-        The Boy, The Mole, The Fox, and The Horse, by Charlie Mackesy
-      </p>
+      {randomQuote && (
+        <>
+          <p className="text-xl font-semibold mx-5">{randomQuote.quote}</p>
+           <p className="italic mt-2 ">
+            {randomQuote.book} by {randomQuote.author}
+          </p>
+        </>
+      )}
     </div>
     <div className="flex flex-col">
       <label className="text-xl">Username</label>
