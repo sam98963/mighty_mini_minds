6f9ea0ddf664fbf7d5f2d815f377153caabab657
Guy Mulligan <guymulligan@outlook.com>
2023-06-26 15:54:59 +0100

    testing tried and failed

--- src/test/MoodMap.test.jsx
+++ src/test/MoodMap.test.jsx
@@ -1,2 +1,19 @@
 import MoodMap from "../pages/MoodMap";
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { it } from 'vitest';
+import test from 'jest';
+
+    // Tests that all components render without crashing
+    it('test_render_components', () => {
+        render(<MoodMap />);
+        expect(screen.getByText('My Week')).toBeInTheDocument();
+        expect(screen.getByText('😊')).toBeInTheDocument();
+        expect(screen.getByText('🙁')).toBeInTheDocument();
+        expect(screen.getByText('🙃')).toBeInTheDocument();
+        expect(screen.getByText('😐')).toBeInTheDocument();
+        expect(screen.getByText('😢')).toBeInTheDocument();
+        expect(screen.getByText('😁')).toBeInTheDocument();
+        expect(screen.getByText('😍')).toBeInTheDocument();
+        expect(screen.getByText('Word of The Day')).toBeInTheDocument();
+        expect(screen.getByText(/"/)).toBeInTheDocument();
+    });
