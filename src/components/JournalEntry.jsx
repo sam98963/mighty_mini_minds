63f049df06fa62485070e424243cc2137c0c404f
Grego12098 <jgregory12098@gmail.com>
2023-06-27 11:29:21 +0100

    journal displaying history correctly

--- src/components/JournalEntry.jsx
+++ src/components/JournalEntry.jsx
@@ -6,13 +6,13 @@ export default function JournalEntry(props) {
     <>
         <h4 className="font-bold ml-5">{props.date}</h4>
         <div className = 'flex flex-col justify-start items-start bg-skin-input mx-4 mb-4 p-2 rounded-lg shadow-md'>
-            <h2 className="font-bold">What did you enjoy today?</h2>
+            <h2 className="font-bold">{props.q1}</h2>
             <p>{props.res1}</p>
             <br/>
-            <h2 className="font-bold">What did you find challenging today?</h2>
+            <h2 className="font-bold">{props.q2}</h2>
             <p>{props.res2}</p>
             <br/>
-            <h2 className="font-bold">What surprised you today?</h2>
+            <h2 className="font-bold">{props.q3}</h2>
             <p>{props.res3}</p>
             <div className='flex flex-row item-start justify-end w-full gap-5 mb-5'>
                 <button className='rounded-md w-10 h-10 bg-skin-secondary text-white  transition-colors duration-300 ease-in-out transform hover:scale-125 '><FontAwesomeIcon icon={faPencil} className='' /></button>
