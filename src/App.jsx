19442768da1eb58f4d41fe61db9b65fe0ce49902
Grego12098 <jgregory12098@gmail.com>
2023-06-29 10:22:55 +0100

    readded everything

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import HomePage from "./pages/HomePage";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import Journal from "./pages/Journal";
+import MoodMap from "./pages/MoodMap";
+import AddEntry from "./pages/AddEntry";
+import WelcomePage from "./pages/WelcomePage";
+import ThanksPage from "./pages/ThanksPage";
+import ProtectedRoutes from "./ProtectedRoute";
+import { AuthProvider } from "./auth/AuthProvider";
+
+function App() {
+  const [theme, setTheme] = useState("");
+
+  const handleThemeChange = (newTheme) => {
+    setTheme(newTheme);
+  };
+  // set up nested router
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<HomePage />}>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route
+            path="appLayout"
+            element={<AppLayout handleThemeChange={handleThemeChange} />}
+          >
+            <Route path="journal" element={<Journal />} />
+            <Route path="moodMap" element={<MoodMap />} />
+            <Route path="addEntry" element={<AddEntry />} />
+            <Route path="welcomePage" element={<WelcomePage />} />
+            <Route path="thanksPage" element={<ThanksPage />} />
+          </Route>
+        </Route>
+      </Route>
+    )
+  );
+  // set up react-query
+  const queryClient = new QueryClient();
+
+  return (
+    <div className={`bg-skin-base ${theme} min-h-screen overflow-hidden`}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
