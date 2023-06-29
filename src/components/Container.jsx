a22d530b7722602663abccd141d7cfa53cb3f49d
Grego12098 <jgregory12098@gmail.com>
2023-06-29 10:21:34 +0100

    deleting main to avoid merge conflict

--- src/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container() {
-  return (
-    <>
-      <div className="container mx-auto bg-white shadow-lg w-32 h-32"></div>
-    </>
-  );
-}
--- src/components/SpeechBubble.jsx
+++
@@ -1,9 +0,0 @@
-export default function SpeechBubble() {
-    return (
-        <div className="rounded-xl">
-            <div className="text-center">
-                <p className="text-base">Word of The Day</p>
-            </div>
-        </div>
-    )
-}
