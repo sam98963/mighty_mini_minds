c261fc4ad450861a843606c55936298100c88231
sam98963 <szwilliamson01@gmail.com>
2023-06-30 14:47:32 +0100

    second email front-end not working - axios post not working

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -1,11 +1,36 @@
 import JournalEntry from "../components/JournalEntry";
 import { useGet } from "../hooks/useGet";
+import { useState } from "react";
 
 
 export default function Journal() {
 const {data: entries, isLoading, isError, error} = useGet();
-
 entries? entries.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)): null;
+const [filteredEntries, setFilteredEntries] = useState(entries);
+function filterEntries(event) {
+  console.log(event.target.value);
+  if (event.target.value === "7Days") {
+    const last7Days = entries.filter((entry) => {
+      const createdAt = new Date(entry.createdAt);
+      const today = new Date();
+      const timeDifference = today - createdAt;
+      const millisecondsInOneDay = 1000 * 60 * 60 * 24;
+      return timeDifference <= 7 * millisecondsInOneDay;
+    });
+    setFilteredEntries(last7Days);
+  } else if (event.target.value === "LastMonth") {
+    const last30Days = entries.filter((entry) => {
+      const createdAt = new Date(entry.createdAt);
+      const today = new Date();
+      const timeDifference = today - createdAt;
+      const millisecondsInOneDay = 1000 * 60 * 60 * 24;
+      return timeDifference <= 30 * millisecondsInOneDay;
+    });
+    setFilteredEntries(last30Days);
+  } else if(event.target.value="All"){
+    setFilteredEntries(entries);
+  }
+}
 
 
     return (
@@ -14,32 +39,31 @@ entries? entries.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)): 
             <h1 className="font-bold text-2xl sm:text-4xl text-center mt-4">Check out your past journals</h1>
             <div className="flex justify-end items-center mt-4">
             <label className="font-bold sm:text-base">Search by</label>
-              <select className="bg-skin-input text-xs sm:text-base mx-4 p-2 rounded-lg shadow-md outline-none">
+              <select className="bg-skin-input text-xs sm:text-base mx-4 p-2 rounded-lg shadow-md outline-none" defaultValue="All" onChange={filterEntries}>
                 <option value="All">All</option>
                 <option value="7Days">Last 7 Days</option>
                 <option value="LastMonth">Last Month</option>
               </select>
               </div>
           <div>
-          {entries? entries.map((entry) => (
-            
-        <JournalEntry
-          key={entry.uuid} // Ensure each entry has a unique key
-          date={new Date(entry.createdAt).toLocaleString('en-GB', {
-            weekday: 'short', // can use 'long'
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit'
-          })} // Convert to DD-MM-YYYY format
-          res1={entry.answer_one}
-          res2={entry.answer_two}
-          res3={entry.answer_three}
-          q1={entry.question_one}
-          q2={entry.question_two}
-          q3={entry.question_three}
-          id={entry.uuid}
+          {filteredEntries? filteredEntries?.map((entry) => (
+              <JournalEntry
+                key={entry.uuid}
+                date={new Date(entry.createdAt).toLocaleString("en-GB", {
+                  weekday: "short",
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+                res1={entry.answer_one}
+                res2={entry.answer_two}
+                res3={entry.answer_three}
+                q1={entry.question_one}
+                q2={entry.question_two}
+                q3={entry.question_three}
+                id={entry.uuid}
         />
       )) : null}
       </div>
